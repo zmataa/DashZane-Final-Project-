d4517bca79e5fda03b9e0b5e5cfa9b6e29aeb2f4
Zane <zmatarieh@bsd220.org>
2024-11-25 09:48:47 -0600

    Start over start simple

---
+++ UserData.swift
@@ -0,0 +1,17 @@
+//
+//  UserData.swift
+//  DashZane (Final Project)
+//
+//  Created by Zane Matarieh on 11/21/24.
+//
+
+import Foundation
+
+struct UserData{
+    var id = UUID() // Unique identifier for the user
+    var mPassword: String
+    var username = ""
+    var password = ""
+    var website = ""
+}
+
